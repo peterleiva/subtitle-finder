fef1b7219356bdb6ec81fe9e2d9e2190e5961ef4
Peter <github@peter.dev.br>
2021-08-29 16:07:59 -0300

    feat: add top-level bin CLI

---
+++ lib/cli.ts
@@ -0,0 +1,37 @@
+import { Command } from 'commander';
+import { version } from '../package.json';
+
+const program = new Command('subtitle-finder');
+
+program
+  .version(version)
+  .description(
+    'Search and download subtitles in most popular providers. The search can be made with a keyword or using a video file'
+  );
+
+program
+  .option(
+    '-v, --verbose',
+    'Make subtitle-finder to verbose during operation. Useful for debugging and see what\'s going on "under the hood"'
+  )
+  .option('-y, --yes', 'download without asking')
+  .option(
+    '-o, --output <path>',
+    'Write output to <path> instead of current directory path'
+  );
+
+program
+  .command(
+    'search <keyword>',
+    'Search for movies and TV Shows by keyword, searching for the best match',
+    { executableFile: __dirname + '/commands/search.js' }
+  )
+  .alias('query')
+  .command(
+    'scan <dir>',
+    'Scan a directory, searching for subtitles for all video files found'
+  );
+
+program.parse(process.argv);
+
+export { program };
