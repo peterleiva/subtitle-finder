a2fb6517191bb67fb05f154d3b8a845270582ed8
Peter <github@peter.dev.br>
2021-09-01 12:28:12 -0300

    feat: add headless environment variable

--- lib/config.ts
+++ lib/config.ts
@@ -11,6 +11,7 @@ const env = {
     username: process.env.OPENSUBTITLE_USERNAME,
     password: process.env.OPENSUBTITLE_PASSWORD_MD5,
   },
+  headless: !['0', 'false'].some(env => env === process.env.HEADLESS),
 };
 
 export default env;
